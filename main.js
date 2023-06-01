c5da6682593b63750c1f108fff0a46bb59dabd43
shalinispk7 <shalinispk7@gmail.com>
2023-06-01 20:58:10 +0530

    function swap

---
+++ main.js
@@ -0,0 +1,11 @@
+// function-swap two numbers
+let num1=3;
+let num2=5;
+document.write("Before swaping:  " +"num1 is: "+num1+ "," + " num2 is: "+num2);
+document.write("<br>");
+function swap(){
+	a=num2;
+	b=num1;
+	document.write("After swaping:  " +"num1 is: "+a+ "," + " num2 is:"+b);
+}
+swap();;
